f22a4467a461dfabac687cead0348b3e3c8a62fc
yetabalij <ermiastsegu@yahoo.com>
2022-09-29 20:34:14 +0300

    home component added to app component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Home from "./components/Home";
+
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello Asbeza.</h1>
+      <Home />
     </div>
   );
 }
