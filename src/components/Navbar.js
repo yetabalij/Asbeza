75e44deb0fa97840400d802c405ed1ea90d05d4d
yetabalij <ermiastsegu@yahoo.com>
2022-09-29 20:55:00 +0300

    basic sideNave setup.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import SideNav, { MenuIcon } from "react-simple-sidenav";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+const Navbar = (props) => {
+  const [showNav, setShowNav] = useState();
+
+  const navItems = [
+    <a target="_blank" href="someLink">
+      Link1
+    </a>,
+    <a target="_blank" href="someLink">
+      Link2
+    </a>,
+    <a target="_blank" href="someLink">
+      Link3
+    </a>,
+  ];
+
+  const title = <h1>Hello octo </h1>;
+
+  return (
+    <div className="bg-red-500 min-h-[7vh] flex items-center">
+      <MenuIcon onClick={() => setShowNav(true)} className="bg-red-500 ml-5" />
+      <SideNav
+        showNav={showNav}
+        onHideNav={() => setShowNav(false)}
+        title={title}
+        items={navItems}
+        navStyle={{ width: "40%" }}
+      />
+    </div>
+  );
 };
 
 export default Navbar;
