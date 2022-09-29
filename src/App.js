d3eda79b2c81c81171a0cd0a5c0b2e07ff8623a0
yetabalij <ermiastsegu@yahoo.com>
2022-09-29 21:12:58 +0300

    routing implemented.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Navbar from "./components/Navbar";
+import History from "./components/History";
+import Profile from "./components/Profile";
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
