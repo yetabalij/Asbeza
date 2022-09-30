c881b9d45bbdaf7684b1e02e9ce1235efd75b7ef
yetabalij <ermiastsegu@yahoo.com>
2022-09-30 21:59:34 +0300

    reduce added.

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./features/productSlice";
+import historyReducer from "./features/historySlice";
 
 export const store = configureStore({
   reducer: {
     products: productReducer,
+    history: historyReducer,
   },
 });
