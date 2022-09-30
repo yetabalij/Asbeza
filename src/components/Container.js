48d5fcb2986e863912c5e37de28dcdf0e1ed5c01
yetabalij <ermiastsegu@yahoo.com>
2022-09-30 16:34:09 +0300

    responsive grid.

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Container = (props) => {
-  return <div className="w-[80%] mx-auto">{props.children}</div>;
+  return (
+    <div className="w-[80%] mx-auto sm:grid sm:grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 mt-5">
+      {props.children}
+    </div>
+  );
 };
 
 export default Container;
