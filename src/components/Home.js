28a68e036670d2199fd72f13b58c444777a34f5c
yetabalij <yetabalij@gmail.com>
2022-09-30 10:29:02 +0300

    container component created.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Container from "./Container";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      Home
+      <Container>Home</Container>
     </div>
   );
 };
