a3066fb9fa00ee62023d952b1736d3eb30e865d7
yetabalij <yetabalij@gmail.com>
2022-09-30 11:26:24 +0300

    bug fixed.

--- src/features/productSlice.js
+++ src/features/productSlice.js
@@ -10,7 +10,7 @@ export const productSlice = createSlice({
   initialState,
   reducers: {
     getData: (state) => {
-      return { products: [...state, data] };
+      return { ...state.products, data };
     },
   },
 });
