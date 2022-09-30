ace347072ce0dd1f269d23206b01f1971f6b9372
yetabalij <yetabalij@gmail.com>
2022-09-30 11:48:29 +0300

    product data successfully featched from the redux store to home page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { getData } from "../features/productSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import Navbar from "./Navbar";
 import Container from "./Container";
 
 const Home = () => {
   const dispatch = useDispatch();
+  const products = useSelector((state) => state.products.data);
+  const [loadin, setLoading] = useState(true);
+
   useEffect(() => {
     dispatch(getData());
+    setLoading(false);
   }, []);
   return (
     <div>
       <Navbar />
-      <Container>Home</Container>
+      {loadin ? (
+        <Container>
+          <h1>...Loading</h1>
+        </Container>
+      ) : (
+        <Container>
+          {products.map((product) => (
+            <div key={product?._id}>
+              <h1>{product?.name}</h1>
+            </div>
+          ))}
+        </Container>
+      )}
     </div>
   );
 };
