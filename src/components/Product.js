6ebc2ed57aa82835700bc0209ee787f26c14380e
yetabalij <ermiastsegu@yahoo.com>
2022-09-30 17:26:05 +0300

    product component completed.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,39 @@
 import React from "react";
+import Rating from "react-rating";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 
 const Product = (props) => {
-  return <div>{props.product.name}</div>;
+  return (
+    <div key={props.key} className="shadow-md">
+      <img src={props.product.image} />
+      <div className="py-2">
+        <h3 className="text-sm font-medium">
+          {props.product.name.toUpperCase()}
+        </h3>
+        <div className="text-orange-500">
+          <Rating
+            emptySymbol={<AiOutlineStar style={{ fontSize: "1.1rem" }} />}
+            fullSymbol={<AiFillStar style={{ fontSize: "1.1rem" }} />}
+            start={0}
+            stop={5}
+            initialRating={props.product?.ratting}
+            readonly
+          />
+        </div>
+        <div className="flex gap-3">
+          <select>
+            <option>0</option>
+            <option>1</option>
+            <option>2</option>
+          </select>
+          <button className="bg-[#046d4c] text-white px-2 py-1">
+            Add To History
+          </button>
+          <p>Price:</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
