eaacd0cb3a961b238636df365193bad3264c5427
yetabalij <ermiastsegu@yahoo.com>
2022-10-01 15:01:01 +0300

    style modified for responsiveness.

--- src/components/History.js
+++ src/components/History.js
@@ -10,13 +10,13 @@ const History = () => {
   return (
     <div>
       <Navbar />
-      <div className="w-[80%] mx-auto flex flex-col mt-4">
+      <div className="w-[80%] lg:w-[60%] mx-auto flex flex-col mt-4">
         <h1 className="font-medium text-2xl mb-4">History</h1>
         {history.map((item, index) => {
           return (
             <div
               key={index}
-              className="w-full shadow-md sm:flex flex-row gap-8 items-center mb-4"
+              className="w-full shadow-md sm:flex flex-row justify-between items-center mb-3"
             >
               <img src={item?.image} className="sm:w-[25%]" />
               <div>
@@ -26,10 +26,10 @@ const History = () => {
                 <p className="font-medium">{`Total Price: $${item?.totalPrice}`}</p>
               </div>
               <div className="flex flex-col items-start">
-                <button className="bg-[#9fcf37] text-white px-2 py-2 mb-2 w-[200px] rounded-sm">
+                <button className="bg-[#9fcf37] text-white px-2 py-2 mb-2 sm:w-[200px] w-full rounded-sm">
                   Delete
                 </button>
-                <button className="bg-[#9fcf37] text-white px-2 py-2 w-[200px] rounded-sm">
+                <button className="bg-[#9fcf37] text-white px-2 py-2 sm:w-[200px] w-full rounded-sm">
                   Place Order
                 </button>
               </div>
