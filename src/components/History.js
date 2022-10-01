2d7a5e53330a3d24dfbc9728a9e42314e46a06a4
yetabalij <ermiastsegu@yahoo.com>
2022-10-01 15:12:10 +0300

    unused code removed.

--- src/components/History.js
+++ src/components/History.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 import Navbar from "./Navbar";
-import Container from "./Container";
 
 const History = () => {
   const history = useSelector((state) => state.history.historyItems);
